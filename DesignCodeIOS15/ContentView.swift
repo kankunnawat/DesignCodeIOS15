c502cf73b391c6fae44e148471220cd9bcbabf2b
kan <40234606+kankunnawat@users.noreply.github.com>
2022-03-13 15:58:14 +0700

    Initial Commit

--- DesignCodeIOS15/ContentView.swift
+++ DesignCodeIOS15/ContentView.swift
@@ -9,13 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(10)
+                .padding(9)
+                .background(.ultraThinMaterial, in:
+                    RoundedRectangle(cornerRadius: 16, style: .continuous))
+                .strokeStyle(cornerRadius: 16)
+
+            Text("SwiftUI for iOS 15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            Text("20 sections - 3 hours".uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundStyle(.secondary)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
+        }
+        .padding(/*@START_MENU_TOKEN@*/ .all, 20.0/*@END_MENU_TOKEN@*/)
+        .padding(.vertical, 20)
+        .frame(height: 350.0)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+//        .cornerRadius(30.0)
+//        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .strokeStyle()
+        .padding(.horizontal, 20)
+        .background(Image("Blob 1").offset(x: 250, y: -100))
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
+        )
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewInterfaceOrientation(.portrait)
+            ContentView()
+                .preferredColorScheme(.dark)
+                .previewInterfaceOrientation(.portrait)
+        }
     }
 }
--- DesignCodeIOS15/DesignCodeIOS15App.swift
+++ DesignCodeIOS15/DesignCodeIOS15App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct DesignCodeIOS15App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AccountView()
         }
     }
 }
